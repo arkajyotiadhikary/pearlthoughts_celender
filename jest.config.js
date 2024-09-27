d3b02ef5adadb46f31201de1c1032208dbfbf557
arkajyotiadhikary <arkajyotiadhikary15@gmail.com>
2024-09-28 00:08:24 +0530

    test changes

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,1 @@
-// jest.setup.js
-import "@testing-library/jest-dom/extend-expect"; // Extends Jest with custom matchers
+import '@testing-library/jest-dom';
